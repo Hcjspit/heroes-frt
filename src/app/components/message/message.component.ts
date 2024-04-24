15015b0bfc5ac1eddab6602d9afa3cc3ebf784ce
Pietro Mizzon <137392041+Hcjspit@users.noreply.github.com>
2024-04-24 09:40:06 +0200

    Add files via upload
    Adding html and TS Component and services, for the run application

---
+++ src/app/components/message/message.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { MessageService } from '../../service/message-service/message.service';
+
+@Component({
+  selector: 'app-message',
+  templateUrl: './message.component.html',
+  styleUrl: './message.component.css',
+})
+export class MessageComponent {
+  constructor(public messageService: MessageService) {}
+}
